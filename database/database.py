29d792061e649d0830fadae50162b927418850e6
Conisla <nthaothion@gmail.com>
2023-07-12 13:45:10 +0400

    code cleaning

--- database/database.py
+++ database/database.py
@@ -1,13 +1,12 @@
 import pyodbc
 import os
 
-# from pandas import DataFrame
-
+# TODO Fix 
 try:
     from database.utils import getConfig
 except:
     from .utils import getConfig
-
+# TODO Fix 
 try:
     server = os.environ["DATABASE_SERVER"]
     database = os.environ["DATABASE_NAME"]
@@ -38,45 +37,10 @@ cursor = cnxn.cursor()
 def sqlRequest(cursor, sql):
     cursor.execute(sql)
     rows = cursor.fetchall()
-    # cursor.close() Where to close cursor ?
     return rows
-
-
-# def sqlRequestDF(sql):
-#     cursor.execute(sql)
-#     df = DataFrame(cursor.fetchall())
-#     print(df.columns)
-#     df_pivot = df.pivot(columns="0")
-#     # df.columns = cursor.description
-#     # return df
-#     return df_pivot
-
-
-# df_test = sqlRequestDF(
-#     "SELECT COUNT(Gender) FROM HumanResources.Employee GROUP BY Gender ORDER BY Gender"
-# )
-# print(df_test)
 
 # # TO TEST IF WORKS
 # version = sqlRequest(cursor, "SELECT @@version;")
 # print(version)
-# test = sqlRequest("SELECT TOP (10) * FROM person.person;")
+# test = sqlRequest(cursor, "SELECT TOP (10) * FROM person.person;")
 # print(test)
-
-# turnover_data = sqlRequest(
-#     "SELECT SUM(UnitPrice) AS Sales , YEAR(OrderDate) as YearOfSale \
-#     FROM "+getConfig("DATABASE")+".dbo.FactInternetSales \
-#     GROUP BY YEAR(OrderDate) ORDER by YearOfSale"
-# )
-# print(turnover_data)
-
-# y = []
-# x = []
-
-# for elt in turnover_data:
-#     # print(elt)
-#     y.append(elt[1])
-#     x.append(float(elt[0]))
-
-# print("x", x)
-# print("y", y)
