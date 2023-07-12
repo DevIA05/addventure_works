6d15b9b9ec43428bfd2f1aa1fed3980c176067c7
Quentin PAYET <quentinpayet1203@gmail.com>
2023-07-12 13:55:39 +0400

    cleaning

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-from database.database import sqlRequest, cnxn
-
 from components.checks import check_database_connection
 from components.plots import display_turnover_per_country, display_turnover_per_year
 
-# from components.test_plots import display_plot_1, display_plot_2, display_plot_3
+
 # Initial page config
 st.set_page_config(
     page_title="AdventureWorks",
@@ -21,11 +19,6 @@ checks_tab, plots_tab = st.tabs(["Checks", "Plots"])
 with checks_tab:
     check_database_connection()
 
-# Tests plots
-# Carefull=> sql query don't match with DW database
-# display_plot_1()
-# display_plot_2()
-# display_plot_3()
 
 # Plots
 with plots_tab:
@@ -35,5 +28,3 @@ with plots_tab:
         display_turnover_per_year()
     with col2:
         display_turnover_per_country()
-# cursor.close()
-# cnxn.close()
